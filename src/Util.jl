b94141d28224495f956dc559a8822d85fb17029d
David Akre <dakre21@gmail.com>
2023-10-30 09:13:54 -0500

    Adding contour plots + first order methods

--- src/Util.jl
+++ src/Util.jl
@@ -11,3 +11,15 @@ function plot_min_of_f(f, x, pts)
         scatter!([pts[i, 1]], [pts[i, 2]], c=c[i], label=l)
     end
 end
+
+function plot_min_of_f(f, x, y, pts)
+    z = @. f(x', y)
+    contour(x, y, z)
+    
+    rows, cols = size(pts)
+    c = distinguishable_colors(rows)
+    for i in 1:rows
+        l = Char(Int('a') + i - 1)
+        scatter!([pts[i, 1]], [pts[i, 2]], c=c[i], label=l)
+    end
+end
